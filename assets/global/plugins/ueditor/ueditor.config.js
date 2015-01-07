0355006894e1e3885f8df6d4bd7da1bed1bdfdcd
Sean Guo <guoxuivy@gmail.com>
2015-01-07 17:29:58 +0800

    ueditor

--- assets/global/plugins/ueditor/ueditor.config.js
+++ assets/global/plugins/ueditor/ueditor.config.js
@@ -23,13 +23,15 @@
     /**
      * 配置项主体。注意，此处所有涉及到路径的配置别遗漏URL变量。
      */
+    console.log(get_url('plug/ueditor'))
     window.UEDITOR_CONFIG = {
 
         //为编辑器实例添加一个路径，这个不能被注释
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: URL + "php/controller.php"
+        , serverUrl: get_url('plug/ueditor')
+        //, serverUrl: URL + "php/controller.php"
         
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
--- protected/controllers/PlugController.php
+++ protected/controllers/PlugController.php
@@ -18,11 +18,67 @@ class PlugController extends \CController {
 	 */
 	public function ueditorAction() {
 		$contro = self::$_PLUGDIR.DIRECTORY_SEPARATOR."ueditor".DIRECTORY_SEPARATOR."controller.php";
-		require_once($contro);
+		//自动处理上传 且返回结果
+		$result = require_once($contro);
+		//水印+缩略图
+		if(isset($result['state'])&&$result['state']=='SUCCESS'&&in_array($result['type'],array('.png','.jpg','.bmp','.gif'))){
+		    $result['thumb'] = $this->imageHandle($result['url'],true);
+		}
+
+		/* 输出结果 */
+		$result = json_encode($result);
+		if (isset($_GET["callback"])) {
+		    if (preg_match("/^[\w_]+$/", $_GET["callback"])) {
+		        echo htmlspecialchars($_GET["callback"]) . '(' . $result . ')';
+		    } else {
+		        echo json_encode(array(
+		            'state'=> 'callback参数不合法'
+		        ));
+		    }
+		} else {
+		    echo $result;
+		}
 	}
 
-	
-    
-    
+	/**
+	 * 自动处理图片 水印 + 缩略图
+	 * @param $fullName
+	 * @return mixed|string
+	 */
+	protected function imageHandle($fullName,$thumb=false,$water=true){
+	    $thumbnail = $fullName;
+	    $path = $_SERVER['DOCUMENT_ROOT'];
+	    if (substr($fullName, 0, 1) != '/') {
+	        $fullName = '/' . $fullName;
+	    }
+	    $path = $path.$fullName;
+
+	    //图片处理类
+	    $tpImage = self::$_PLUGDIR.DIRECTORY_SEPARATOR."ueditor".DIRECTORY_SEPARATOR."tpImage.php";
+		require_once($tpImage);
+
+	    $image = new tpImage();
+	    $image2 = clone $image;
+	    //生成缩略图
+	    if($thumb){
+	        $image->open($path);
+	        $tmp = pathinfo($path);
+	        $tmp = $tmp['dirname'].'/'.$tmp['filename'].'_thumb.'.$tmp['extension'];
+	        $image->thumb(200,200);
+	        $image->save($tmp);
+	        $thumbnail = pathinfo($fullName);
+	        $thumbnail = $thumbnail['dirname'].'/'.$thumbnail['filename'].'_thumb.'.$thumbnail['extension'];
+	    }
+	    $watermark=__ROOT__."/public/img/water.png";
+	    //生成水印
+	    if($water && $watermark && file_exists($watermark)){
+	        $image2->open($path);
+	        $image2->water($watermark, 9);
+	        $image2->save($path);
+	    }
+
+	    return $thumbnail;
+	}
+
 	
 }
